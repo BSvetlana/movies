066c413ebf50cd75196c7fb7d08df3958486bae8
BSvetlana <svetlanabisacki@gmail.com>
2018-04-11 20:06:04 +0200

    "fourth commit"

---
+++ resources/views/layouts/master.blade.php
@@ -0,0 +1,57 @@
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta name="description" content="">
+    <meta name="author" content="">
+    <link rel="icon" href="../../favicon.ico">
+
+    <title>Sticky Footer Navbar Template for Bootstrap</title>
+    <link rel="canonical" href="https://getbootstrap.com/docs/4.0/examples/sticky-footer-navbar/">
+
+    <!-- Bootstrap core CSS -->
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-alpha.6/css/bootstrap.min.css" integrity="sha384-rwoIResjU2yc3z8GV/NPeZWAv56rSmLldC3R/AZzGRnGxQQKnKkoFVhFQhNUwEyJ" crossorigin="anonymous">
+
+    <!-- Custom styles for this template -->
+    <link href="sticky-footer-navbar.css" rel="stylesheet">
+</head>
+
+<body>
+
+<!-- Fixed navbar -->
+<nav class="navbar navbar-toggleable-md navbar-inverse fixed-top bg-inverse">
+    <button class="navbar-toggler navbar-toggler-right" type="button" data-toggle="collapse" data-target="#navbarCollapse" aria-controls="navbarCollapse" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+    </button>
+    <a class="navbar-brand" href="#"></a>
+    <div class="collapse navbar-collapse" id="navbarCollapse">
+        <ul class="navbar-nav mr-auto">
+
+            <li class="nav-item active">
+                <a class="nav-link" href="#">Movies</a>
+            </li>
+            <li class="nav-item active">
+                <a class="nav-link" href="#">Add Movie</a>
+            </li>
+        </ul>
+        <form class="form-inline mt-2 mt-md-0">
+            <input class="form-control mr-sm-2" type="text" placeholder="Search">
+            <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
+        </form>
+    </div>
+</nav>
+
+<!-- Begin page content -->
+@yield('content')
+
+
+
+
+<!-- Bootstrap core JavaScript
+================================================== -->
+<!-- Placed at the end of the document so the pages load faster -->
+
+</body>
+</html>
--- resources/views/movies/index.blade.php
+++ resources/views/movies/index.blade.php
@@ -1,8 +1,17 @@
-<ul>
-    @foreach($movies as $movie)
-        <li>
-            <a href="{{route('single-movies',['id'=>$movie->id])}}">{{$movie->title}}</a>
-            <p>{{$movie->storyline}}</p>
-        </li>
-    @endforeach
-</ul>
+@extends('layouts.master')
+
+@section('content')
+<div class="container">
+    <div class="mt-3">
+        <h1>Movies</h1>
+    </div>
+    <ul>
+        @foreach($movies as $movie)
+            <li>
+                <a href="{{route('single-movies',['id'=>$movie->id])}}">{{$movie->title}}</a>
+                <p>{{$movie->storyline}}</p>
+            </li>
+        @endforeach
+    </ul>
+</div>
+@endsection
